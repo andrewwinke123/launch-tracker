ccfcb53f7fac4e7f72b282a15b7d21a19edc1cdd
andrewwinke123 <andrewwinke@gmail.com>
2023-05-28 23:52:00 -0400

    setting up launches index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+class Rocket:
+  def __init__(self, mission, model, mfg, size, orbit, crew, payload, location, date):
+    self.mission = mission
+    self.model = model
+    self.mfg = mfg
+    self.size = size
+    self.orbit = orbit
+    self.crew = crew
+    self.payload = payload
+    self.location = location
+    self.date = date
+
+rockets = [
+  Rocket('ROCKET LIKE A HURRICANE', 'Electron', 'Rocket Lab', 'small', 'LEO', 0, 'NASA Tropics', 'NZ', 'May 8, 2023'),
+  Rocket('ROCKET LIKE A HURRICANE', 'Electron', 'Rocket Lab', 'small', 'LEO', 0, 'NASA Tropics', 'NZ', 'May 8, 2023'),
+  Rocket('ROCKET LIKE A HURRICANE', 'Electron', 'Rocket Lab', 'small', 'LEO', 0, 'NASA Tropics', 'NZ', 'May 8, 2023'),
+  Rocket('ROCKET LIKE A HURRICANE', 'Electron', 'Rocket Lab', 'small', 'LEO', 0, 'NASA Tropics', 'NZ', 'May 8, 2023'),
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello 🚀</h1>')
@@ -9,3 +29,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def launch_index(request):
+  return render(request, 'launches/index.html', { 'rockets': rockets })
