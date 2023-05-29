6e1fa15d51705f78991d048358fd331cab7cc620
andrewwinke123 <andrewwinke@gmail.com>
2023-05-28 22:26:20 -0400

    setting up home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello 🚀</h1>')
